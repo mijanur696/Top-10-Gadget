f4a9351923655e23a8addefeef5bd7e8563dc01d
Md Mijanur Rahaman <mijanur696@gmail.com>
2021-05-17 21:24:00 +0530

    major changes
    add chart as a price tag

---
+++ script.js
@@ -0,0 +1,43 @@
+var ctx=document.getElementById("myChart").getContext("2d");
+var myChart=new Chart(ctx,{
+
+    type:"bar",
+    data:{
+        labels:['Jan','Feb','Mar','Apr','May',],
+        datasets:[{
+          label:'Amazon Price',
+          data:[10000,20000,45000,72560,50000,],
+          backgroundColor:'#FF9900',
+        },
+        {
+          label:'Flipkart Price',
+          data:[36000,26000,36000,78560,60000,],
+          backgroundColor:'#F8E831',
+        },   
+      ], 
+    },
+
+    options:{
+        responsive:true,
+        layout:{
+          padding:{
+              top:20,
+              bottom:30
+          }
+        },
+        plugins:{
+          title:{
+            display:true,
+            text:"Price Chart 2021",
+            position:'bottom',
+            color:'green',
+            font:{
+              size:22
+            }
+          }
+        }
+      },
+
+})
+
+
